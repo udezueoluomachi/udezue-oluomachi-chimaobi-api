bde13fb3d810ed842aba491fe2f03e38f526d03c
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2022-01-21 12:08:27 +0100

    beta features added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.createServer( (req, res) => {
     });
 
     fs.readFile("./data.json", (err, data) => {
-        if(err) throw err;
+        if(err) throw err
         res.write(data)
         res.end()
     })
